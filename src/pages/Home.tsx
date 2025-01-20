8819ef24ccf50ba878deb2829c8fbf0346078290
CanerYesiltas <caneryesiltas1@gmail.com>
2025-01-20 18:53:51 -0500

    Add API data fetching logic with Redux integration and error handling improvements

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,30 +1,43 @@
-import React, { useState } from 'react'
-import SearchComp from '../components/SearchComp.tsx'
-import axios from 'axios'
+import React, { useEffect, useState } from 'react';
+import SearchComp from '../components/SearchComp.tsx';
+import axios from 'axios';
+import { fetchFail, fetchStart, getSuccesProduct, Product } from '../features/productsSlice.ts';
+import { useAppDispatch } from '../app/hooks.ts';
+
+export interface Products {
+  products: Product[];
+  total: number;
+  skip: number;
+  limit: number;
+}
 
 const Home = () => {
-  const [search, setSearch] = useState("")
-
+  const [search, setSearch] = useState('');
+  const dispatch = useAppDispatch ()
 
   const getData = async () => {
-
+  dispatch(fetchStart())
     try {
-      
-      const {data} = await axios.get(`https://dummyjson.com/products/search?q=${search}`)
-
+      const { data } = await axios.get<Products>(
+        `https://dummyjson.com/products/search?q=${search}`,
+      );
+      dispatch(getSuccesProduct(data.products))
     } catch (error) {
-      
+      dispatch(fetchFail())
     }
+  };
 
-
-  }
+  useEffect(() => {
+    getData()
+  }, [search])
+  
 
 
   return (
     <div>
-      <SearchComp/>
+      <SearchComp />
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
