4f905c57886d3a6b05cd17ef935136c3dcf73c8a
CanerYesiltas <caneryesiltas1@gmail.com>
2025-01-21 16:24:56 -0500

    Add loading and error state conditions

--- src/models/models.ts
+++ src/models/models.ts
@@ -1,2 +1,21 @@
+export type EventFunc = (e: React.ChangeEvent<HTMLInputElement>) => void;
 
-export type EventFunc = (e:React.ChangeEvent<HTMLInputElement>) => void; 
+export interface Products {
+  products: Product[];
+  total: number;
+  skip: number;
+  limit: number;
+}
+export interface Product {
+  id: number;
+  title: string;
+  description: string;
+  price: number;
+  discountPercentage: number;
+  rating: number;
+  stock: number;
+  brand: string;
+  category: string;
+  thumbnail: string;
+  images: string[];
+}
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,48 +1,53 @@
-import React, { ChangeEvent, useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import SearchComp from '../components/SearchComp.tsx';
 import axios from 'axios';
-import { fetchFail, fetchStart, getSuccesProduct, Product } from '../features/productsSlice.ts';
-import { useAppDispatch } from '../app/hooks.ts';
-import { EventFunc } from '../models/models.ts';
-
-export interface Products {
-  products: Product[];
-  total: number;
-  skip: number;
-  limit: number;
-}
+import { fetchFail, fetchStart, getSuccesProduct,} from '../features/productsSlice.ts';
+import { useAppDispatch, useAppSelector } from '../app/hooks.ts';
+import { EventFunc, Products } from '../models/models.ts';
 
 const Home = () => {
   const [search, setSearch] = useState('');
-  const dispatch = useAppDispatch ()
+  const dispatch = useAppDispatch();
+  const {loading, error, productsList} = useAppSelector(state => state.products)
 
   const getData = async () => {
-  dispatch(fetchStart())
+    dispatch(fetchStart());
     try {
       const { data } = await axios.get<Products>(
         `https://dummyjson.com/products/search?q=${search}`,
       );
-      dispatch(getSuccesProduct(data.products))
+      dispatch(getSuccesProduct(data.products));
     } catch (error) {
-      dispatch(fetchFail())
+      dispatch(fetchFail());
     }
-    
   };
 
   useEffect(() => {
-    getData()
-  }, [search])
-  
-// const handleChange = (e:React.ChangeEvent<HTMLInputElement>) =>{
-// setSearch(e.target.value)
+    getData();
+  }, [search]);
 
-const handleChange:EventFunc = (e) => {
-  setSearch(e.target.value)
-}
+  const handleChange: EventFunc = (e) => {
+    setSearch(e.target.value);
+  };
 
   return (
     <div>
       <SearchComp handleChange={handleChange} />
+      {loading ? (
+        <div className="mt-52">
+          <p className="text-center text-white-50">Products Loading...</p>
+        </div>
+      ) : error ? (
+        <div className="mt-52">
+          <p className="text-center text-white-50">Something Went Wrong...</p>
+        </div>
+      ) : (
+        <div>
+          {productsList.map((item) => (
+            <p key={item.id}>{item.title}</p>
+          ))}
+        </div>
+      )}
     </div>
   );
 };
